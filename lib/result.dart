39b6382f4c9f5e7559ed7453e7bc5bcd9fd07728
Aditya Chandra <adityachandra9898@gmail.com>
2021-07-01 00:40:29 +0530

    lec56_reset

--- lib/result.dart
+++ lib/result.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   // const Result({ Key? key }) : super(key: key);
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
   String get resultPhrase {
     var resultText = 'You did it!';
     if (resultScore <= 80) {
@@ -20,10 +21,18 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            child: Text('Im not Satisfied with my result'),
+            onPressed: resetHandler,
+          ),
+        ],
       ),
     );
   }
